3547124f52f446c8978f418584a5dcf6f7d81026
nickolasrm <nickolas.machado@ipiranga.ipiranga>
2022-09-06 16:16:35 +0000

    replaced configureproject by bootstrapproject
    Signed-off-by: nickolasrm <nickolas.machado@ipiranga.ipiranga>

--- tests/test_test_kedro.py
+++ tests/test_test_kedro.py
@@ -1,6 +1,12 @@
-"""Just to trigger conftest."""
+"""TestKedro tests."""
+from pathlib import Path
+from kedro.framework.session import KedroSession
+from kedro_pytest.test_kedro import TestKedro
+from kedro_pytest.plugin import tkedro as tkedro_fixture  # noqa: F401
 
 
-def test_dummy():
-    """Ignore me."""
-    assert True
+def test_session(tkedro: TestKedro):
+    """Tests if a session can be instantiated using TestKedro"""
+    tkedro.new('proj')
+    with KedroSession.create('proj', Path.cwd()) as session:
+        assert session is not None
